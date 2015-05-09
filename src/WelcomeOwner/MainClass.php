3831c0d9ef2eb8adeae440ba2a94292b61b6f89d
Blubberboy333 <thecowmooman@gmail.com>
2015-05-09 09:12:06 -0400

    Update MainClass.php

--- src/WelcomeOwner/MainClass.php
+++ src/WelcomeOwner/MainClass.php
@@ -1,28 +1,30 @@
 <?php
 
-namespace HelloOwner;
+namespace WelcomeOwner;
 
-use pocketmine\event\Listener;
-use pocketmine\event\PlayerJoinEvent;
-use pocketmine\utils\Config;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\permission\ServerOperator;
+use pocketmine\Player;
+use pocketmine\Server;
 
-class MainClass extends PluginBase implements Listener {
-  public function onEnable(){
-    $this->saveDefaultConfig();
-  }
-  public function onDisable(){
-    //empty
-  }
-  
-  public function onJoin(PlayerJoinEvent $event){
-    $name = $player->getName();
-    $owner = $this->getConfig->get("Owner");
-    $coowner = $this->getConfig->get("CoOwner");
-    if($name == $owner){
-      $this->broadcastMessage("The server Owner has joined the game.");
-    }elseif($name == $coowner){
-      $this->broadcastMessage("The server Co Owner has joined the game.");
+class MainClass extends PluginBase implements Listener{
+	public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		if(!file_exists($this->getDataFolder() . "config.yml")){
+			@mkdir($this->getDataFolder());
+			file_put_contents($this->getDataFolder() . "config.yml",$this->getResource("config.yml"));
+		}
+	}
+    public function onJoin(PlayerJoinEvent $event){
+		  $player = $event->getPlayer();
+          $owner = $this->getConfig()->get("Owner");
+	  	$name = $player->getDisplayName();
+	  	if($name == $owner){
+		  	$this->getServer()->broadcastMessage("The server owner has joined the game.");
+	  	}else{
+			
+	  	}
     }
-  }
 }
